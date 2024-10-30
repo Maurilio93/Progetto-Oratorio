8ef24e97fe17376bcdbe06bfe69986b9ddd98749
Nunzia Biele <nunzia.biele@virgilio.it>
2024-10-30 19:04:15 +0100

    pull and merge

--- sanvincenzo/src/Section/LandingPage.jsx
+++ sanvincenzo/src/Section/LandingPage.jsx
@@ -135,31 +135,9 @@ export function LandingPage() {
             {/* Sezione delle card vuote al centro */}
             <div className="flex justify-center items-center gap-6 mt-6">
                 <Partite />
-                <Card className="w-96 animate-pulse">
-                    <CardHeader shadow={false} floated={false} className="relative grid h-56 place-items-center bg-gray-300">
-                        <iframe src='https://www.tuttocampo.it/WidgetV2/Partita/30018696-716a-4194-a931-e7547a88ff56' width='500' height='350' loading='lazy'></iframe>
-                    </CardHeader>
-                    <CardBody>
-                        <Typography as="div" variant="h1" className="mb-4 h-3 w-56 rounded-full bg-gray-300">&nbsp;</Typography>
-                    </CardBody>
-                    <CardFooter className="pt-0">
-                        <Button disabled tabIndex={-1} className="h-8 w-20 bg-gray-300 shadow-none hover:shadow-none">&nbsp;</Button>
-                    </CardFooter>
-                </Card>
+                
 
-                <Card className="w-96 animate-pulse">
-                    <CardHeader shadow={false} floated={false} className="relative grid h-56 place-items-center bg-gray-300">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="h-12 w-12 text-gray-500">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                        </svg>
-                    </CardHeader>
-                    <CardBody>
-                        <Typography as="div" variant="h1" className="mb-4 h-3 w-56 rounded-full bg-gray-300">&nbsp;</Typography>
-                    </CardBody>
-                    <CardFooter className="pt-0">
-                        <Button disabled tabIndex={-1} className="h-8 w-20 bg-gray-300 shadow-none hover:shadow-none">&nbsp;</Button>
-                    </CardFooter>
-                </Card>
+
             </div>
 
             {/* Sezione delle ultime notizie */}
