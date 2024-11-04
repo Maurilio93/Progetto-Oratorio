a7b5ebe37a39489d70011f361e0ff0658ec9fcac
Nunzia Biele <nunzia.biele@virgilio.it>
2024-11-04 12:06:55 +0100

    made responsive the landing-page

--- sanvincenzo/src/Section/LandingPage.jsx
+++ sanvincenzo/src/Section/LandingPage.jsx
@@ -44,22 +44,16 @@ export function LandingPage() {
 
   return (
     <div className="bg-white">
-      <header className="bg-blue-400 fixed top-0 left-0 right-0 z-50">
-        <div className="max-w-screen-xl mx-auto flex items-center justify-between h-16 px-4 md:px-0">
-          {/* Logo e titolo sulla sinistra */}
-          <div className="flex items-center gap-2 md:gap-3 lg:gap-4">
-            <img
-              src="images/IMG_6743.PNG"
-              alt="Logo"
-              className="h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14 rounded-xl"
-            />
-            <h4 className="text-white font-semibold text-xs sm:text-sm md:text-base">
-              Oratorio San Vincenzo c5
-            </h4>
-          </div>
-          <div className="hidden md:flex items-center md:mx-4 lg:mx-10">
+     <header className="bg-blue-400 fixed top-0 left-0 right-0 z-50">
+    <div className="max-w-screen-xl mx-auto flex items-center justify-between h-16 px-4 md:px-0">
+        {/* Logo e titolo sulla sinistra */}
+        <div className="flex items-center gap-2 md:gap-4">
+            <img src="images/IMG_6743.PNG" alt="Logo" className="h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14 rounded-xl" />
+            <h4 className="text-white font-semibold text-xs sm:text-sm md:text-base">Oratorio San Vincenzo c5</h4>
+        </div>
+        <div className="hidden md:flex items-center md:mx-10">
             <nav aria-label="Global">
-              <ul className="flex gap-6 text-sm">
+                <ul className="flex gap-14 text-sm">
                 <li
                   className="relative"
                   onMouseEnter={handleMouseEnterSocieta}
@@ -87,7 +81,11 @@ export function LandingPage() {
                     </svg>
                   </button>
                   {isSocietaDropdownOpen && (
-                    <div className="absolute z-10 bg-white font-bold divide-y divide-gray-100 rounded-lg shadow w-44 max-w-full left-1/2 transform -translate-x-1/2 mt-2">
+                    <div
+                      className="absolute z-10 bg-white font-bold divide-y divide-gray-100 rounded-lg shadow w-44 left-1/2 transform -translate-x-1/2 mt-2"
+                      onMouseEnter={handleMouseEnterSocieta}
+                      onMouseLeave={handleMouseLeaveSocieta}
+                    >
                       <ul className="py-2 text-sm text-blue-400">
                         <li>
                           <a
@@ -152,7 +150,11 @@ export function LandingPage() {
                     </svg>
                   </button>
                   {isSquadreDropdownOpen && (
-                    <div className="absolute z-10 bg-white font-bold divide-y divide-gray-100 rounded-lg shadow w-44 max-w-full left-1/2 transform -translate-x-1/2 mt-2">
+                    <div
+                      className="absolute z-10 bg-white font-bold divide-y divide-gray-100 rounded-lg shadow w-44 left-1/2 transform -translate-x-1/2 mt-2"
+                      onMouseEnter={handleMouseEnterSquadre}
+                      onMouseLeave={handleMouseLeaveSquadre}
+                    >
                       <ul className="py-2 text-sm text-blue-400">
                         <li>
                           <a
@@ -219,148 +221,61 @@ export function LandingPage() {
         </div>
 
         {isMobileMenuOpen && (
-          <div className="md:hidden absolute top-16 left-0 right-0 bg-blue-400 text-white text-center">
-            <ul className="flex flex-col items-center space-y-4 py-4">
-              <li
-                className="relative"
-                onMouseEnter={handleMouseEnterSocieta}
-                onMouseLeave={handleMouseLeaveSocieta}
-              >
+    <div className="md:hidden absolute top-16 left-0 right-0 bg-blue-400 text-white text-center">
+        <ul className="flex flex-col items-center space-y-4 py-4">
+            <li className="relative" onMouseEnter={handleMouseEnterSocieta} onMouseLeave={handleMouseLeaveSocieta}>
                 <button className="font-bold flex items-center justify-center">
-                  SOCIETÀ
-                  <svg
-                    className="w-2.5 h-2.5 ml-1"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 1 4 4 4-4"
-                    />
-                  </svg>
+                    SOCIETÀ
+                    <svg className="w-2.5 h-2.5 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+                    </svg>
                 </button>
                 {isSocietaDropdownOpen && (
-                  <div className="absolute z-10 bg-white font-bold divide-y divide-gray-100 rounded-lg shadow w-44 max-w-full left-1/2 transform -translate-x-1/2 mt-2">
-                    <ul className="py-2 text-sm text-blue-400">
-                      <li>
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100"
-                        >
-                          DIRIGENZA
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100"
-                        >
-                          LA NOSTRA STORIA
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
+                    <div
+                        className="absolute z-10 bg-white font-bold divide-y divide-gray-100 rounded-lg shadow w-44 left-1/2 transform -translate-x-1/2 mt-2"
+                        onMouseEnter={handleMouseEnterSocieta}
+                        onMouseLeave={handleMouseLeaveSocieta}
+                    >
+                        <ul className="py-2 text-sm text-blue-400">
+                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">DIRIGENZA</a></li>
+                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">LA NOSTRA STORIA</a></li>
+                        </ul>
+                    </div>
                 )}
-              </li>
-              <li>
-                <a href="#" className="font-bold">
-                  PARTNER
-                </a>
-              </li>
-              <li>
-                <a href="#" className="font-bold">
-                  GALLERIA
-                </a>
-              </li>
-              <li
-                className="relative"
-                onMouseEnter={handleMouseEnterSquadre}
-                onMouseLeave={handleMouseLeaveSquadre}
-              >
+            </li>
+            <li><a href="#" className="font-bold">PARTNER</a></li>
+            <li><a href="#" className="font-bold">GALLERIA</a></li>
+            <li className="relative" onMouseEnter={handleMouseEnterSquadre} onMouseLeave={handleMouseLeaveSquadre}>
                 <button className="font-bold flex items-center justify-center">
-                  SQUADRE
-                  <svg
-                    className="w-2.5 h-2.5 ml-1"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 1 4 4 4-4"
-                    />
-                  </svg>
+                    SQUADRE
+                    <svg className="w-2.5 h-2.5 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+                    </svg>
                 </button>
                 {isSquadreDropdownOpen && (
-                  <div className="absolute z-10 bg-white font-bold divide-y divide-gray-100 rounded-lg shadow w-44 max-w-full left-1/2 transform -translate-x-1/2 mt-2">
-                    <ul className="py-2 text-sm text-blue-400">
-                      <li>
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100"
-                        >
-                          PRIMA SQUADRA
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100"
-                        >
-                          UNDER 19
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
+                    <div
+                        className="absolute z-10 bg-white font-bold divide-y divide-gray-100 rounded-lg shadow w-44 left-1/2 transform -translate-x-1/2 mt-2"
+                        onMouseEnter={handleMouseEnterSquadre}
+                        onMouseLeave={handleMouseLeaveSquadre}
+                    >
+                        <ul className="py-2 text-sm text-blue-400">
+                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">PRIMA SQUADRA</a></li>
+                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">UNDER 19</a></li>
+                        </ul>
+                    </div>
                 )}
-              </li>
-              <li>
-                <button
-                  onClick={() => navigate("/contatti")}
-                  className="font-bold"
-                >
-                  CONTATTI
-                </button>
-              </li>
-            </ul>
-            <div className="flex justify-center gap-6 mt-4 pb-4">
-              <a
-                href="https://www.facebook.com/people/Oratorio-San-Vincenzo-c5/61565515795683/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-black hover:text-gray-300"
-              >
-                <FaFacebook size={24} />
-              </a>
-              <a
-                href="https://www.instagram.com/oratorio_sanvincenzoc5/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-black hover:text-gray-300"
-              >
-                <FaInstagram size={24} />
-              </a>
-              <a
-                href="https://www.youtube.com/@OratorioSanVincenzoC5"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-black hover:text-gray-300"
-              >
-                <FaYoutube size={24} />
-              </a>
-            </div>
-          </div>
-        )}
-      </header>
+            </li>
+            <li><button onClick={() => navigate("/contatti")} className="font-bold">CONTATTI</button></li>
+        </ul>
+        <div className="flex justify-center gap-6 mt-4 pb-4">
+            <a href="https://www.facebook.com/people/Oratorio-San-Vincenzo-c5/61565515795683/" target="_blank" rel="noopener noreferrer" className="text-black hover:text-gray-300"><FaFacebook size={24} /></a>
+            <a href="https://www.instagram.com/oratorio_sanvincenzoc5/" target="_blank" rel="noopener noreferrer" className="text-black hover:text-gray-300"><FaInstagram size={24} /></a>
+            <a href="https://www.youtube.com/@OratorioSanVincenzoC5" target="_blank" rel="noopener noreferrer" className="text-black hover:text-gray-300"><FaYoutube size={24} /></a>
+        </div>
+    </div>
+)}
+</header>
+
 
       <div className="flex flex-col justify-center items-center mt-10 md:mt-0">
         <div
