9ec4b1af4d110726a21dc50beb141f7f8fdcbb1e
Maurilio Valenti <maurilio.valenti@gmail.com>
2024-11-05 15:57:38 +0100

    added story section and added photos in the gallery section

--- sanvincenzo/src/Section/Galleria.jsx
+++ sanvincenzo/src/Section/Galleria.jsx
@@ -8,80 +8,61 @@ export function Galleria() {
     const data = [
         {
             imgelink:
-                "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+                "images/gallery1.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+                "images/gallery2.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+                "images/gallery3.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
+                "images/gallery4.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+                "images/gallery5.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+                "images/gallery6.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+                "images/gallery7.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+                "images/gallery8.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
+                "images/gallery9.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+                "images/gallery10.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+                "images/gallery11.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
+                "images/gallery12.png",
         },
         {
             imgelink:
-                "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+                "images/gallery13.png",
         },
-        {
-            imgelink:
-                "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        },
-        {
-            imgelink:
-                "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-        },
-        {
-            imgelink:
-                "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
-        },
-        {
-            imgelink:
-                "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
-        },
-        {
-            imgelink:
-                "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
-        },
+
     ];
 
     const [active, setActive] = React.useState(
-        "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        "images/Progetto senza titolo (3).png",
     );
 
     return (
--- sanvincenzo/src/Section/LaNostraStoria.jsx
+++ sanvincenzo/src/Section/LaNostraStoria.jsx
@@ -1,55 +1,49 @@
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Typography,
-} from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
 
 export function LaNostraStoria() {
   const navigate = useNavigate();
+
   return (
-    <div className="flex justify-center items-center my-3 px-4 overflow-auto rounded-2xl-">
-      <Card className="w-full max-w-8xl flex flex-col md:flex-row">
-        <CardHeader
-          shadow={false}
-          floated={false}
-          className="m-0 md:w-1/2 w-full shrink-0 rounded-b-none md:rounded-r-none"
-        >
-          <img
-            src="images/WhatsApp Image 2024-10-29 at 16.37.06.jpeg"
-            alt="card-image"
-            className="h-full w-full object-cover"
-          />
-        </CardHeader>
-        <CardBody className="flex flex-col justify-center p-6">
-          <div className="flex items-center justify-center w-full max-w-4xl mt-4">
-            <button
-              onClick={() => navigate("/")}
-              type="button"
-              className="bg-white text-blue-600 font-semibold py-1 px-3 rounded-md text-sm hover:bg-gray-200 absolute mr-96"
-            >
-              ⬅️
-            </button>
-            <h1 className="text-xl font-semibold my-6 bg-white text-blue-400 py-2 px-6 rounded-lg shadow-lg md:left-4">
-              LA NOSTRA STORIA
-            </h1>
-          </div>
-          <Typography color="gray" className="mb-8 font-normal text-sm md:text-base">
-            L&apos;ASD Oratorio San Vincenzo nasce con un obiettivo chiaro: promuovere il calcio a 5 all&apos;interno del quartiere Resuttana San Lorenzo, valorizzando i giovani e favorendo la crescita sportiva e personale dei ragazzi. Fin dalle sue origini, la società ha trovato la sua forza nei legami costruiti all&apos;interno dell&apos;oratorio, dove tanti giovani talenti hanno avuto l’opportunità di crescere e di cimentarsi nello sport.
+    <div className="relative bg-blue-400 p-8 md:p-16">
+      {/* Bottone per tornare indietro */}
+      <button
+        onClick={() => navigate("/")}
+        type="button"
+        className="bg-white text-blue-600 font-semibold py-1 px-3 rounded-md text-sm hover:bg-gray-200 absolute top-6 left-8 md:top-4 md:left-16 md:mt-10"
+      >
+        ⬅️
+      </button>
 
-            Dopo anni di lavoro e dedizione, il nostro impegno ha portato a risultati straordinari, grazie alla dedizione di diverse figure fondamentali, culminati nella storica promozione al Campionato Regionale Siciliano di Serie C2 nella stagione 2023/24. Guidati dal mister Marco De Francisci, sotto la supervisione di Matteo Cicero, vera bandiera dell&apos;oratorio, che da oltre un decennio allena e supporta i nostri giovani atleti, abbiamo affrontato un campionato intenso e pieno di sfide. La nostra squadra, composta da giovani promesse e supportata da alcuni veterani, è riuscita a emergere come una delle formazioni più dinamiche e combattive, guadagnandosi il soprannome di “i ragazzini terribili” della Serie D.
+      {/* Contenitore per titolo e logo centrati */}
+      <div className="flex items-center justify-center mt-8 mb-8 md:-mt-12">
+        <h2 className="text-lg md:text-2xl font-semibold bg-white text-blue-400 py-1 px-4 rounded-lg shadow-lg mr-4">
+          LA NOSTRA STORIA
+        </h2>
+        <img
+          src="images/IMG_6743.PNG" // Sostituisci con il percorso del logo
+          alt="Logo Oratorio San Vincenzo"
+          className="w-20 h-20 md:w-24 md:h-24" // Dimensioni del logo
+        />
+      </div>
 
-            L’ASD Oratorio San Vincenzo è più di una semplice squadra; è una famiglia che si unisce attorno ai valori dello sport, della comunità e della crescita collettiva. Grazie al supporto di figure chiave come Padre Calogero Di Fiore e il custode Carmelo Guzzo, possiamo allenarci nei campi in sintetico all’interno dell’oratorio, continuando a portare avanti il nostro progetto nel cuore del contesto parrocchiale.
-
-            Guardando alla stagione 2024/25, le ambizioni non mancano. Coach Matteo Cicero, ora anche Direttore Sportivo, insieme al Direttore Generale Mario Ferrante, giovane dirigente di grande valore, punta a consolidare la posizione della squadra e a competere per il titolo. La nostra visione non è solo per la prima squadra: crediamo fortemente nel nostro vivaio e nel progetto a lungo termine di costruire una cantera locale, un settore giovanile che sia il cuore pulsante della nostra società.
-
-            Con umiltà e determinazione, affrontiamo questa nuova sfida in Serie C2, consapevoli della forza del nostro gruppo e della passione che ci unisce.
-          </Typography>
-          <a href="#" className="inline-block">
-          </a>
-        </CardBody>
-      </Card>
+      {/* Testo con paragrafi distanziati */}
+      <div className="text-base md:text-lg text-blue-400 font-bold leading-relaxed rounded-3xl p-8 bg-gray-100 space-y-6">
+        <p>
+          L&apos;ASD Oratorio San Vincenzo nasce con un obiettivo chiaro: promuovere il calcio a 5 all&apos;interno del quartiere Resuttana San Lorenzo, valorizzando i giovani e favorendo la crescita sportiva e personale dei ragazzi. Fin dalle sue origini, la società ha trovato la sua forza nei legami costruiti all&apos;interno dell&apos;oratorio, dove tanti giovani talenti hanno avuto l’opportunità di crescere e di cimentarsi nello sport. 👟
+        </p>
+        <p>
+          Dopo anni di lavoro e dedizione, il nostro impegno ha portato a risultati straordinari, grazie alla dedizione di diverse figure fondamentali, culminati nella storica promozione al Campionato Regionale Siciliano di Serie C2 nella stagione 2023/24. Guidati dal mister Marco De Francisci, sotto la supervisione di Matteo Cicero, vera bandiera dell&apos;oratorio, che da oltre un decennio allena e supporta i nostri giovani atleti, abbiamo affrontato un campionato intenso e pieno di sfide. La nostra squadra, composta da giovani promesse e supportata da alcuni veterani, è riuscita a emergere come una delle formazioni più dinamiche e combattive, guadagnandosi il soprannome di “i ragazzini terribili” della Serie D.🏆
+        </p>
+        <p>
+          L’ASD Oratorio San Vincenzo è più di una semplice squadra; è una famiglia che si unisce attorno ai valori dello sport, della comunità e della crescita collettiva. Grazie al supporto di figure chiave come Padre Calogero Di Fiore e il custode Carmelo Guzzo, possiamo allenarci nei campi in sintetico all’interno dell’oratorio, continuando a portare avanti il nostro progetto nel cuore del contesto parrocchiale.🤝
+        </p>
+        <p>
+          Guardando alla stagione 2024/25, le ambizioni non mancano. Coach Matteo Cicero, ora anche Direttore Sportivo, insieme al Direttore Generale Mario Ferrante, giovane dirigente di grande valore, punta a consolidare la posizione della squadra e a competere per il titolo. La nostra visione non è solo per la prima squadra: crediamo fortemente nel nostro vivaio e nel progetto a lungo termine di costruire una cantera locale, un settore giovanile che sia il cuore pulsante della nostra società.🎯
+        </p>
+        <p>
+          Con umiltà e determinazione, affrontiamo questa nuova sfida in Serie C2, consapevoli della forza del nostro gruppo e della passione che ci unisce.🔥
+        </p>
+      </div>
     </div>
   );
 }
